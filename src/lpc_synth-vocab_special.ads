5b6d0b2aa1e78903e6eb2bc492f4aa6b0ebd508f
Fabien Chouteau <fabien.chouteau@gmail.com>
2022-08-20 11:28:31 +0200

    Import vocabs from the Talkie project

---
+++ src/lpc_synth-vocab_special.ads
@@ -0,0 +1,7 @@
+--  Converted to Ada from the Talkie Arduino project:
+--  https://github.com/ArminJo/Talkie
+package LPC_Synth.Vocab_Special is
+   pragma Style_Checks (Off);
+   spPAUSE1 : aliased constant LPC_Synth.LPC_Data := (16#10#, 16#28#, 16#83#, 16#bb#, 16#a2#, 16#26#, 16#c0#, 16#00#, 16#3f#, 16#52#, 16#6a#, 16#64#, 16#5c#, 16#60#, 16#50#);
+   spPAUSE2 : aliased constant LPC_Synth.LPC_Data := (16#10#, 16#28#, 16#83#, 16#bb#, 16#a2#, 16#26#, 16#c0#, 16#00#, 16#00#, 16#00#, 16#03#, 16#ff#, 16#52#, 16#62#, 16#8a#, 16#9c#, 16#9e#, 16#a8#, 16#50#);
+end LPC_Synth.Vocab_Special;
--- tests/src/tests.adb
+++ tests/src/tests.adb
@@ -1,40 +1,29 @@
-with LPC_Synth;
 with GNAT.OS_Lib;
+
+with LPC_Synth;
+with LPC_Synth.Vocab_US_TI99; use LPC_Synth.Vocab_US_TI99;
 
 procedure Tests is
    D : LPC_Synth.Instance;
    Output : LPC_Synth.Out_Array (1 .. 1024);
 
-   Data : aliased constant LPC_Synth.LPC_Data :=
-     (16#00#, 16#10#, 16#B6#, 16#43#, 16#23#, 16#1B#, 16#2A#, 16#9C#, 16#BC#,
-      16#CE#, 16#B5#, 16#B6#, 16#8A#, 16#3B#, 16#2F#, 16#14#, 16#3D#, 16#E4#,
-      16#22#, 16#8E#, 16#CC#, 16#3D#, 16#71#, 16#90#, 16#86#, 16#A4#, 16#B3#,
-      16#0F#, 16#73#, 16#E4#, 16#2A#, 16#1A#, 16#E8#, 16#D4#, 16#20#, 16#7A#,
-      16#ED#, 16#02#, 16#39#, 16#37#, 16#09#, 16#96#, 16#BB#, 16#58#, 16#71#,
-      16#CE#, 16#3E#, 16#A5#, 16#D0#, 16#76#, 16#1C#, 16#63#, 16#AE#, 16#B1#,
-      16#75#, 16#0D#, 16#95#, 16#18#, 16#FB#, 16#EC#, 16#5B#, 16#25#, 16#84#,
-      16#C5#, 16#41#, 16#0A#, 16#16#, 16#C0#, 16#E2#, 16#51#, 16#5A#, 16#42#,
-      16#65#, 16#B2#, 16#34#, 16#93#, 16#54#, 16#70#, 16#98#, 16#ED#, 16#0D#,
-      16#23#, 16#11#, 16#AC#, 16#24#, 16#3B#, 16#63#, 16#28#, 16#B4#, 16#44#,
-      16#F9#, 16#16#, 16#D0#, 16#42#, 16#30#, 16#8F#, 16#26#, 16#47#, 16#AE#,
-      16#0E#, 16#DC#, 16#13#, 16#A9#, 16#79#, 16#65#, 16#93#, 16#37#, 16#04#,
-      16#E4#, 16#1C#, 16#4D#, 16#8C#, 16#AD#, 16#A1#, 16#7F#, 16#17#, 16#11#,
-      16#CB#, 16#1A#, 16#48#, 16#5E#, 16#C1#, 16#86#, 16#74#, 16#E8#, 16#92#,
-      16#00#, 16#00#, 16#F0#, 16#17#, 16#4C#);
+   Data_List : constant array (Natural range <>)
+     of LPC_Synth.LPC_Data_Const_Acc
+       := (spt_HELLO'Access,
+           spt_READY_TO_START'Access);
 
    Ignore : Integer;
-
 begin
-   LPC_Synth.Set_Data (D, Data'Unrestricted_Access);
 
-   loop
-      LPC_Synth.Next_Points (D, Output);
+   for Word of Data_List loop
+      LPC_Synth.Set_Data (D, Word);
+      while LPC_Synth.Has_Data (D) loop
+         LPC_Synth.Next_Points (D, Output);
 
-      for Elt of Output loop
-         Ignore := GNAT.OS_Lib.Write
-           (GNAT.OS_Lib.Standout, Elt'Address, Elt'Size / 8);
+         for Elt of Output loop
+            Ignore := GNAT.OS_Lib.Write
+              (GNAT.OS_Lib.Standout, Elt'Address, Elt'Size / 8);
+         end loop;
       end loop;
-
-      exit when not LPC_Synth.Has_Data (D);
    end loop;
 end Tests;
